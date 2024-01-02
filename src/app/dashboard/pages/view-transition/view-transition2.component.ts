fc64b99c61ef7d5d212707edc682b83e3a769039
Danny Armijos <daniel2667@hotmail.es>
2024-01-02 17:51:14 +0100

    fon de los controles, triggers y transtions

---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TittleComponent} from '@shared/tittle/tittle.component';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, TittleComponent],
+  template: `
+  <app-tittle title="View transition"/>
+  <section class="flex justify-start">
+    <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300">
+    <div class="bg-blue-500 w-56 h-56">
+
+    </div>
+
+  </section>
+  `,
+})
+export default class ViewTransitionComponent {
+
+}
